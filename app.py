6ff301ae6e5e2f420fc178a8443bb68eb70bb45b
HSIN YUNCHIEH <hsinyunchieh@gmail.com>
2022-04-05 15:31:42 +0800

    更新

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from distutils.log import debug
-from flask import Flask #載入Flask
-import datetime
-app =Flask(__name__)    #建立application 物件
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
 
-#建立網站首頁回應方式
-@app.route("/")
-def index():    #用來回應網站首頁連線的含式
-    return "Hello ,this is doiya"    #回傳網站首頁內容
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/datetime")
-def date():    
-    return str(datetime.datetime.now())
-#啟動網站伺服器
-
-if __name__=="__main__":
-    app.run()(host='127.0.0.1',port=5000,debug=True)
+@app.route('/portfolio-details.html')
+def show__profile_details():
+    return render_template('portfolio-details.html')
+        
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
